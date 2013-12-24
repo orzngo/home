0e9abdf3616aa13183105ae2650067e93e67f038
Morio-Yamada <morio_yamada@dwango.co.jp>
2013-12-24 22:28:58 +0900

    gitconfigもリポジトリに入れた

--- setup.sh
+++ setup.sh
@@ -2,10 +2,12 @@ DIR=$(cd $(dirname $0); pwd)
 
 rm -rf $DIR/../.vimrc ; ln -s $DIR/.vimrc $DIR/../.vimrc
 rm -rf $DIR/../.zshrc ; ln -s $DIR/.zshrc $DIR/../.zshrc
+echo \"fixme > $DIR/../.zshrc.include
 rm -rf $DIR/../.bash_profile ; ln -s $DIR/.bash_profile $DIR/../.bash_profile
 rm -rf $DIR/../.bashrc ; ln -s $DIR/.bashrc $DIR/../.bashrc
+rm -rf $DIR/../.gitconfig ; ln -s $DIR/.bashrc $DIR/../.gitconfig
+echo \"fixme > $DIR/../.gitconfig.include
 
-echo \"fixme > $DIR/../.zshrc.include
 
 rm -rf $DIR/../.vim
 mkdir $DIR/../.vim
